6e3fcc1df9868b4cdc3fa15de2074aa11cc5ad72
hydroperfox <hydroperfox@gmail.com>
2024-09-12 17:36:45 -0300

    Update

--- crates/whack/src/commandprocesses/check_process.rs
+++ crates/whack/src/commandprocesses/check_process.rs
@@ -1,9 +1,25 @@
+use crate::packagemanager::*;
+use colored::*;
+
 pub fn check_process(matches: &clap::ArgMatches) {
     let builtins = matches.get_one::<std::path::PathBuf>("builtins");
     let package = matches.get_one::<String>("package");
 
-    match crate::packagemanager::retrieve_dag(package.cloned()) {
-    }
+    let dag = match Dag::retrieve(package.cloned()) {
+        Ok(dag) => dag,
+        Err(error) => {
+            match error {
+                DagError::ManifestNotFound => {
+                    println!("{} {}", "Error:".red(), "Whack manifest not found.");
+                },
+                DagError::PackageMustBeSpecified => {
+                    println!("{} {}", "Error:".red(), "Package must be specified.");
+                },
+            }
+
+            return;
+        },
+    };
 
     // Check for the built-ins first
     fixme();
--- crates/whack/src/packagemanager/dag.rs
+++ crates/whack/src/packagemanager/dag.rs
@@ -1,30 +1,33 @@
 use std::rc::Rc;
 use crate::packagemanager::WhackPackage;
 
-/// Retrieves the directed acyclic graph of the dependency tree.
-pub fn retrieve_dag(package: Option<String>) -> Result<Dag, DagError> {
-    // Read the Whack manifest
-    fixme();
-
-    // If current project is a workspace, then require a package name
-    // to be specified, at which the check process executes.
-    fixme();
-
-    // Check for manifest updates.
-    fixme();
-
-    // If the manifest has been updated, update dependencies
-    // and clear up the build script's artifacts.
-    // Remember that the lock file must be considered for the
-    // exact versions of registry dependencies.
-    fixme();
-
-    // Build a directed acyclic graph (DAG) of the dependencies.
-    fixme();
-}
-
+/// Directed acyclic graph of the dependency tree.
 pub struct Dag {
     pub edges: Vec<DagEdge>,
+}
+
+impl Dag {
+    /// Retrieves the directed acyclic graph of the dependency tree.
+    pub fn retrieve(package: Option<String>) -> Result<Dag, DagError> {
+        // Read the Whack manifest
+        fixme();
+
+        // If current project is a workspace, then require a package name
+        // to be specified, at which the check process executes.
+        fixme();
+
+        // Check for manifest updates.
+        fixme();
+
+        // If the manifest has been updated, update dependencies
+        // and clear up the build script's artifacts.
+        // Remember that the lock file must be considered for the
+        // exact versions of registry dependencies.
+        fixme();
+
+        // Build a directed acyclic graph (DAG) of the dependencies.
+        fixme();
+    }
 }
 
 pub struct DagEdge {
