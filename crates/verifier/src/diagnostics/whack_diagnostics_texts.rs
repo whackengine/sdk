9e6e7806fd67cc838797be92cca30c2941702a2c
Matheus Dias de Souza <HydroperFox@gmail.com>
2024-11-28 10:31:00 -0300

    Update whack_diagnostics_texts.rs

--- crates/verifier/src/diagnostics/whack_diagnostics_texts.rs
+++ crates/verifier/src/diagnostics/whack_diagnostics_texts.rs
@@ -88,7 +88,7 @@ lazy_static! {
         WhackDiagnosticKind::CannotExtendFinalClass.id() => "Cannot extend final class $1.".into(),
         WhackDiagnosticKind::ExtendingSelfReferentialClass.id() => "Extending self-referential class.".into(),
         WhackDiagnosticKind::NotAnInterface.id() => "Not an interface.".into(),
-        WhackDiagnosticKind::OptionsClassMustExtendObject.id() => "Options class must extend Object.".into(),
+        WhackDiagnosticKind::OptionsClassMustExtendObject.id() => "RecordLike class must extend Object.".into(),
         WhackDiagnosticKind::MalformedEventMetadata.id() => "Malformed Event meta-data.".into(),
         WhackDiagnosticKind::AbstractMethodMustBeOverriden.id() => "Abstract method must be overriden: $1.".into(),
         WhackDiagnosticKind::AbstractGetterMustBeOverriden.id() => "Abstract getter must be overriden: $1.".into(),
